b1aca6d38df6793249609ac496b09210bb5913c3
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2025-01-13 21:46:01 +0100

    chore: remove sql file

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-    "id" INTEGER PRIMARY KEY,
-    "display_name" TEXT NOT NULL,
-    "vanity_url" TEXT,
-    "avatar" TEXT,
-    "frame" TEXT,
-    "created_at" TEXT NOT NULL,
-    "updated_at" TEXT
-);
